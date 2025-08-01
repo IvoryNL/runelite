d8d66d426b7eb64e6d9f80a2949d1bd534dcfa88
Ivan <ivan.franken89@gmail.com>
2025-08-01 12:54:25 +0200

    PotionFlipper: fix incorrect application of tax.

--- runelite-client/src/main/java/net/runelite/client/plugins/potionFlipper/Helpers/ProfitCalculatorHelper.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/potionFlipper/Helpers/ProfitCalculatorHelper.java
@@ -1,14 +1,12 @@
 package net.runelite.client.plugins.potionFlipper.Helpers;
 
-import net.runelite.client.plugins.potionFlipper.ItemPrice;
-
 public final class ProfitCalculatorHelper
 {
     public static int calculateProfit3To4(int threeDosePrice, int fourDosePrice)
     {
-        var priceThreeDoseTimesToFourDoses = threeDosePrice / 3 * 4;
+        var priceThreeDoseToFourDoses = threeDosePrice / 3 * 4;
         var tax = 1.02f;
 
-        return (int)((fourDosePrice - priceThreeDoseTimesToFourDoses) / tax);
+        return (int)((fourDosePrice / tax) - priceThreeDoseToFourDoses);
     }
 }
